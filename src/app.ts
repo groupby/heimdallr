db6bf53ba36c81ec0d0782176e7e4d0f5a1e9009
Victoria Johns <victoria.johns@groupbyinc.com>
2017-03-31 12:24:42 -0400

    Add logging page

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,8 @@ export class App {
     config.map([
       { route: '', redirect: 'dashboard' },
       { route: 'dashboard', name: 'dashboard', moduleId: 'dashboard', nav: true, title: 'Dashboard' },
-      { route: 'admin', name: 'admin', moduleId: 'admin', nav: true, title: 'Admin' }
+      { route: 'admin', name: 'admin', moduleId: 'admin', nav: true, title: 'Admin' },
+      { route: 'logging', name: 'logging', moduleId: 'logging', nav: true, title: 'Logging'}
     ]);
 
     this.router = router;
